25ef4983c5b28bb55aa2372a7279faf4791ce9f4
zyad231 <90551362+zyad231@users.noreply.github.com>
2025-05-28 14:08:53 +0300

    generated migrations

--- obj/Debug/net8.0-windows/EF_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/EF_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0268d07eee9cc75326f3439b10ecfc96d90412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0041f6e3889e413d83264c21f6f5f4065b1b5de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
